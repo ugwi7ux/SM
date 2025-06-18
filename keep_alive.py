4c4418a256f91a767a9dab40b297e69935fc673f
ugwi7ux <sjadalqysy767@gmail.com>
2025-06-19 02:09:28 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Keep-alive is active"
+    return "🚀 الخادم يعمل بنجاح"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
+    Thread(target=run, daemon=True).start()
